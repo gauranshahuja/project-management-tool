38f3cf2b930fc1bf552d929eee01624f01a99138
Gauransh Ahuja <gauransh3637ahuja@gmail.com>
2026-05-29 11:35:36 +0000

    correcting theapi issue

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://project-management-tool-9yl1.onrender.com", 
+  baseURL: "https://project-management-tool-9yl1.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
